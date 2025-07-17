daa4dc5f1f1d0adf2b04b73acf4f8eb845c255c0
Cao Văn Nam <caovannamutt@gmail.com>
2025-07-17 20:19:18 +0700

    Update nutiphone.js

--- nutiphone.js
+++ nutiphone.js
@@ -7,15 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var emailInput = document.querySelector('input[name="email"]');
     if (emailInput) {
       var path = window.location.pathname;
+      console.log('Current path:', path); // Thêm debug
       if (path.endsWith('capcutpro.html')) {
         emailInput.value = "tkcapcutpro@gmail.com";
       } else if (path.endsWith('chatgpt.html')) {
         emailInput.value = "tkcanva@gmail.com";
       } else if (path.endsWith('canva.html')) {
         emailInput.value = "tkchatpgt@gmail.com";
-      } else if (path.endsWith('chatgpt.html')) {
+      } else {
         emailInput.value = "emailkhac@gmail.com"; 
       }
+    } else {
+      console.log('Không tìm thấy input name="email"');
     }
+  } else {
+    console.log('Không phải thiết bị iOS');
   }
 });
